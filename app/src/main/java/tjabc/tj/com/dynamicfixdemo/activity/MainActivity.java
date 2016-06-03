cad61728d429487e4123e45a66db8c94fade6b5d
李晓光 <lxg@ziteng.cn>
2016-06-03 16:44:19 +0800

    init commit

--- app/src/main/java/tjabc/tj/com/dynamicfixdemo/activity/MainActivity.java
+++ app/src/main/java/tjabc/tj/com/dynamicfixdemo/activity/MainActivity.java
@@ -2,6 +2,7 @@ package tjabc.tj.com.dynamicfixdemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Log.i("tag","DynamicFixDemo");
         testBtn.setOnClickListener(this);
     }
 
